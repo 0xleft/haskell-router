7995926f832884d78bdb0009d7a57355a8a38e4a
left <107749872+0xleft@users.noreply.github.com>
2025-09-17 21:30:01 +0200

    More project structure

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # haskell-router
+
+A router written in haskell
+
+## Dev dependencies
+
+```bash
+# wsl
+sudo apt-get install libpcap-dev
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
+import Router.Packet (LinkPacket(..), IPPacket(..))
+
+import qualified Data.ByteString.Char8 as B
+import Network.Pcap
 
 main :: IO ()
-main = someFunc
+main = do
+  -- open device
+  dev <- openLive "lo" 65535 False 0
+  setFilter dev "ether proto 0xFFFF" True 0
+
+  -- send
+  sendPacketBS dev (B.pack "\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\255\255Hello, World!")
+
+  -- receive
+  loopBS dev (-1) (\_ packet -> putStrLn (show packet))
+  return ()
