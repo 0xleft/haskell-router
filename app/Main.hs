3b8bb49ac6bdb17bc1b24c7e5332e8d30fcb7066
left <107749872+0xleft@users.noreply.github.com>
2025-09-18 13:26:16 +0200

    refactor and add types

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,8 @@
 module Main (main) where
 
-import Router.Packet (LinkPacket(..), IPPacket(..))
-
+import Router.Packet (Packet(..), PacketQueue(..))
 import qualified Data.ByteString.Char8 as B
 import Network.Pcap
-
 
 -- main thread will be processing one packet at the time from the queue
 -- a thread will be listening for packets and adding them to the queue
