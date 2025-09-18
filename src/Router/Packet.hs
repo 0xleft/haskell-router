5d65d3c10342ff9d7c5d2437887530ae108a443e
Floris Buijnsters <florisbuijnsters@gmail.com>
2025-09-18 14:21:03 +0200

    Tested it with GHCI, I should probably figure out how to do test

--- src/Router/Packet.hs
+++ src/Router/Packet.hs
@@ -1,7 +1,7 @@
 module Router.Packet (
   LinkPacket(..),
   IPPacket(..),
-  PakcetType
+  PacketType(..),
   getPacketType
 ) where
 
@@ -30,8 +30,9 @@ data IPPacket = IPPacket {
 }
 
 
-data PacketType = IPv6Packet | IPv4Packet
 
+data PacketType = IPv6Packet | IPv4Packet
+-- Gives the correct Word16 type for the data packet
 getPacketType :: PacketType -> Word16
 getPacketType IPv6Packet = 35037 -- IPv6 is bit value 88DD Which is 35037 in decimal
 getPacketType IPv4Packet = 2048 -- IPv4 is bit value 0800 which is 2048 in decimal
