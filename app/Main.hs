9df79b057955c09c8924af85f1be385db0708718
0xleft <107749872+0xleft@users.noreply.github.com>
2025-09-21 10:36:40 +0200

    Clean up

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,6 @@ import Router.Services.Beacon as Beacon ( start )
 import Router.Services.Receiver as Receiver ( start )
 import Router.Services.Sender as Sender ( start )
 import Control.Concurrent (forkIO)
-import Network.Pcap (openLive, sendPacket)
-import qualified Router.Packer as Packer
-import qualified Router.Packet as Packet
-import qualified Router.Layers as Layers
-import qualified Router.Layers.Ethernet as Ethernet
-import Foreign (free)
 
 -- main thread will be processing one packet at the time from the queue
 -- a thread will be listening for packets and adding them to the queue
@@ -18,23 +12,6 @@ import Foreign (free)
 main :: IO ()
 main = do
   let pq = PacketQueue { maxLength = 1000, packets = [] } -- todo replace with MVar
-
-      eth = Layers.Ethernet [0] [0] Ethernet.IPv4Packet
-      ethernetLayer = Layers.PacketLinkLayer (Layers.LinkLayerEth eth)
-      ip = Layers.Ip 0 0 0 0 0 0 0 0 0 0 [] ethernetLayer
-      ipLayer = Layers.PacketNetworkLayer (Layers.NetworkLayerIp ip)
-      tcp = Layers.Tcp 0 0 0 0 0 0 0 0 0 0 0 ipLayer
-      tcpLayer = Layers.PacketTransferLayer (Layers.TransferLayerTcp tcp)
-      packet = Packet.Packet tcpLayer
-
-  (ptr, len, ws) <- Packer.pack packet
-
-  putStrLn $ show ws
-
-  interface <- openLive "wlo1" 65535 False 0
-  sendPacket interface ptr len
-
-  free ptr
 
   _ <- forkIO $ Beacon.start -- thread
   _ <- forkIO $ Receiver.start pq  -- thread
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Router.Layers.Ethernet as Ethernet
 import Router.Layers as Layers
 import Router.Packet as Packet
 import Router.Packer as Packer
+import Foreign (free)
 
 main :: IO ()
 main = hspec $ do
@@ -22,18 +23,24 @@ main = hspec $ do
     it "Should " $ do -- todo actualy finish this test
       let eth = Layers.Ethernet [0, 0, 0, 0, 0, 0] [0, 0, 0, 0, 0, 0] Ethernet.IPv4Packet
           ethernetLayer = Layers.PacketLinkLayer (Layers.LinkLayerEth eth)
-          ip = Layers.Ip 0 0 0 0 0 0 0 0 0 0 [] ethernetLayer
+          ip = Layers.Ip 0x45 0 0 0 0 0 0 0 0 0 [] ethernetLayer
           ipLayer = Layers.PacketNetworkLayer (Layers.NetworkLayerIp ip)
           tcp = Layers.Tcp 0 0 0 0 0 0 0 0 0 0 0 ipLayer
           tcpLayer = Layers.PacketTransferLayer (Layers.TransferLayerTcp tcp)
-
-          
 
           packet = Packet.Packet tcpLayer
           fields = Packer.getFields packet
           recursiveFields = Packer.getRecursiveFields (topLayer packet)
           convertedFields = Packer.convertFields recursiveFields 
 
-      _ <- putStrLn (Prelude.show convertedFields)
+      (ptr, len, ws) <- Packer.pack packet
+
+      putStrLn $ Prelude.show ws
+
+      -- interface <- openLive "wlo1" 65535 False 0
+      -- sendPacket interface ptr len
+
+      free ptr
+
       -- _ <- putStrLn (Prelude.show fields)
       True `shouldBe` True
